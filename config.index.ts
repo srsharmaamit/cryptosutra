b8e8bdf90a7ccbeceb932451adff5ded5ce2138a
Amit <sr.amitsharma@gmail.com>
2023-01-18 20:12:31 +0000

    updating Configuration

--- config.index.ts
+++ config.index.ts
@@ -1,25 +1,45 @@
-import { writeFile } from 'fs';
-
 import { name, version } from './package.json';
 
-const targetPath = './src/environments/environment.prod.ts';
-
-const envConfigFile = `export const environment = {
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.prod.ts';
+  // Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: 'src/environments/.env',
+  });
+  // `environment.ts` file structure
+  // @ts-ignore
+  const envConfigFile = `export const environment = {
    production: true,
    firebaseConfig: {
-        apiKey: '${process.env['FIREBASE_API_KEY']}'
+        apiKey: '${process.env.FIREBASE_API_KEY}'
     },
     name: '${name}',
     version: '${version}',
-    title: '${process.env['TITLE']}'
+    title: '${process.env.TITLE}'
 };
 `;
+  console.log(
+    colors.magenta(
+      'The file `environment.prod.ts` will be written with the following content: \n'
+    )
+  );
+  writeFile(targetPath, envConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(
+        colors.magenta(
+          `Angular environment.ts file generated correctly at ${targetPath} \n`
+        )
+      );
+    }
+  });
+};
 
-console.log(
-  `THIS IS A VERIFICATION OF GITHUB ACTION SECRET ACCESS:  ${process.env['TITLE']}`
-);
-writeFile(targetPath, envConfigFile, 'utf8', (err) => {
-  if (err) {
-    return console.log(err);
-  }
-});
+setEnv();
