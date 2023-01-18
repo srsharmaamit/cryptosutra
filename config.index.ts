54c0c6009618aa486a83ec5d56e6fa1a4bed09b0
Amit <sr.amitsharma@gmail.com>
2023-01-18 13:22:25 +0000

    updating Configuration

--- config.index.ts
+++ config.index.ts
@@ -11,10 +11,13 @@ const envConfigFile = `export const environment = {
     },
     name: '${name}',
     version: '${version}',
-    title: '${process.env['title']}'
+    title: '${process.env['TITLE']}'
 };
 `;
 
+console.log(
+  `THIS IS A VERIFICATION OF GITHUB ACTION SECRET ACCESS:  ${process.env['TITLE']}`
+);
 writeFile(targetPath, envConfigFile, 'utf8', (err) => {
   if (err) {
     return console.log(err);
